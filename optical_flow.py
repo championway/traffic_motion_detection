784095575349507652806a883f5e42585a5fa76e
championway <cpwearth.eed03@g2.nctu.edu.tw>
2020-02-08 02:45:33 +0800

    motion detector

---
+++ optical_flow.py
@@ -0,0 +1,113 @@
+import imutils
+import cv2
+import numpy as np
+import time
+
+# =============================================================================
+# USER-SET PARAMETERS
+# =============================================================================
+
+# Number of frames to pass before changing the frame to compare the current
+# frame against
+FRAMES_TO_PERSIST = 10
+
+# Minimum boxed area for a detected motion to count as actual motion
+# Use to filter out noise or small objects
+MIN_SIZE_FOR_MOVEMENT = 2000
+
+# Minimum length of time where no motion is detected it should take
+#(in program cycles) for the program to declare that there is no movement
+MOVEMENT_DETECTED_PERSISTENCE = 100
+
+# =============================================================================
+# CORE PROGRAM
+# =============================================================================
+
+
+# Create capture object
+cap = cv2.VideoCapture(5) # Flush the stream
+cap.release()
+cap = cv2.VideoCapture(0) # Then start the webcam
+
+# Init frame variables
+first_frame = None
+next_frame = None
+
+# Init display font and timeout counters
+font = cv2.FONT_HERSHEY_SIMPLEX
+delay_counter = 0
+movement_persistent_counter = 0
+
+def draw_flow(img, flow, step=16):
+    
+    #from the beginning to position 2 (excluded channel info at position 3)
+    h, w = img.shape[:2]
+    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
+    fx, fy = flow[y,x].T
+    lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    cv2.polylines(vis, lines, 0, (0, 255, 0))
+    for (x1, y1), (x2, y2) in lines:
+        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    return vis
+
+
+def draw_hsv(flow):
+    (h, w) = flow.shape[:2]
+    (fx, fy) = (flow[:, :, 0], flow[:, :, 1])
+    ang = np.arctan2(fy, fx) + np.pi
+    v = np.sqrt(fx * fx + fy * fy)
+    hsv = np.zeros((h, w, 3), np.uint8)
+    hsv[..., 0] = ang * (180 / np.pi / 2)
+    hsv[..., 1] = 0xFF
+    hsv[..., 2] = np.minimum(v * 4, 0xFF)
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    cv2.imshow('hsv', bgr)
+    return bgr
+
+
+def warp_flow(img, flow):
+    (h, w) = flow.shape[:2]
+    flow = -flow
+    flow[:, :, 0] += np.arange(w)
+    flow[:, :, 1] += np.arange(h)[:, np.newaxis]
+    res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
+    return res
+
+(ret, prev) = cap.read()
+
+prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+show_hsv = False
+show_glitch = False
+cur_glitch = prev.copy()
+
+while True:
+    (ret, img) = cap.read()
+    vis = img.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    flow = cv2.calcOpticalFlowFarneback(prevgray,gray,None,0.5,5,15,3,5,1.1,cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
+    prevgray = gray
+    cv2.imshow('flow', draw_flow(gray, flow))
+    if show_hsv:
+        gray1 = cv2.cvtColor(draw_hsv(flow), cv2.COLOR_BGR2GRAY)
+        thresh = cv2.threshold(gray1, 25, 0xFF,
+                               cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cv2.imshow('thresh', thresh)
+        cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # loop over the contours
+        for c in cnts:
+
+            # if the contour is too small, ignore it
+            (x, y, w, h) = cv2.boundingRect(c)
+            if w > 100 and h > 100 and w < 900 and h < 680:
+                cv2.rectangle(vis, (x, y), (x + w, y + h), (0, 0xFF, 0), 4)
+                cv2.putText(vis,str(time.time()),(x, y),cv2.FONT_HERSHEY_SIMPLEX,1,(0, 0, 0xFF),1)
+        cv2.imshow('Image', vis)
+    if show_glitch:
+        cur_glitch = warp_flow(cur_glitch, flow)
+        cv2.imshow('glitch', cur_glitch)
+    ch = 0xFF & cv2.waitKey(5)
+cv2.destroyAllWindows()
